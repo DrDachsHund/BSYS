740843fcd899454ab4fb36a8f6e33e422494b743
Marvin Klett <Marvin.Klett@htwg-konstanz.de>
2018-10-30 14:01:30 +0100

    update

--- Kapitel5/Kapitel5A8.c
+++ Kapitel5/Kapitel5A8.c
@@ -5,26 +5,31 @@
 
 int main (int argc, char* argv[])
 {
-    int status;
-    pid_t pid;
-
-    int rc = fork();
-	if (rc < 0)
-	{
-		// fork faild exit
-		fprintf(stderr, "fork failed\n");
-		exit(1);
-		
-	} else if (rc == 0)
-	{
-        //child
-        printf("im child pid:%d\n",(int) getpid());
-    } else
-    {
-        //parent
-        printf("im parent pid:%d\n",(int) getpid());
-    }  
-
+   
+   int fd[2], nbytes;
+   pid_t childpid;
+   char string[] = "Hello new Process!\n";
+   char readbuffer[80];
+   
+   pipe(fd);
+   
+   if ((childpid = fork()) == -1) {
+   	perror("fork");
+   	exit(1);
+   }
+   
+   if (childpid == 0)
+   {
+   	close(fd[1]);
+   	nbytes = read(fd[0], readbuffer, sizeof(string) + 1);
+   	printf("Received string: %s", readbuffer);
+   	exit(0);
+   } else 
+   {
+   	close(fd[0]);
+   	write(fd[1], string, strlen(string) + 1);
+   	wait(0);
+   }
     return 0;
 
 } 
